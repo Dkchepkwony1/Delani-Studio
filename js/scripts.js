88dcbfb4031de0c2fc57c0f693b74cc5d72d16cc
Dickson Chepkwony <dickchep@yahoo.com>
2023-04-14 23:04:53 +0300

    This commit improves the outlay of the webpage through editing html,css and adding jQuery code.

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,10 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
+
+
+$(document).ready(function(){
+  $(":button").click(function(){
+    alert("The paragraph was clicked.");
+  });
+});

4fb4b928c5314bab9598bc67745e3e00ae5a34ae
Dickson Chepkwony <dickchep@yahoo.com>
2023-04-15 11:48:32 +0300

    This commit further improves page layout using css styles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var person1Input= $("input#person1").val();
-    var person2Input= $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput= $("input#exclamation").val();
-    var verbInput= $("input#verb").val();
-    var nounInput= $("input#noun").val();
-    var flavorInput= $("input:radio[name=flavor]:checked").val();
-    console.log(flavorInput);
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-    $(".flavor").text(flavorInput);
-    $("#story").show();
-    event.preventDefault();
-  });
-});
+// $(document).ready(function() {
+//   $("#blanks form").submit(function(event) {
+//     var person1Input= $("input#person1").val();
+//     var person2Input= $("input#person2").val();
+//     var animalInput= $("input#animal").val();
+//     var exclamationInput= $("input#exclamation").val();
+//     var verbInput= $("input#verb").val();
+//     var nounInput= $("input#noun").val();
+//     var flavorInput= $("input:radio[name=flavor]:checked").val();
+//     console.log(flavorInput);
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+//     $(".flavor").text(flavorInput);
+//     $("#story").show();
+//     event.preventDefault();
+//   });
+// });
 
 
 $(document).ready(function(){
